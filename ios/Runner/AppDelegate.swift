c9706967e11c5887066cc7b293c5579b8e218f2c
takeru.hashimoto <hstake25@gmail.com>
2020-07-17 19:54:54 +0900

    push notification 設定

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -13,4 +13,14 @@ import Flutter
     }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+  
 }
+
+func application(_ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    var readableToken: String = ""
+    for i in 0..<deviceToken.count {
+      readableToken += String(format: "%02.2hhx", deviceToken[i] as CVarArg)
+    }
+    print("Received an APNs device token: \(readableToken)") 
+  }
